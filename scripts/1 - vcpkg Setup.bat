fe266ebe0ecde476c2325bc36671bdccda947891
boolCoder99 <boolCoder99@123.de>
2025-04-02 19:13:37 +0200

    added script for setting additional environment variables

--- scripts/1 - vcpkg Setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title vcpkg Setup
-
-echo Getting vcpkg files...
-git clone https://github.com/microsoft/vcpkg.git
-
-echo Installing vcpkg...
-cd vcpkg
-bootstrap-vcpkg.bat -disableMetrics
--- scripts/setup visual studio.ps1
+++ scripts/setup visual studio.ps1
@@ -1,7 +1,5 @@
 $SOURCE_PATH = "../"
-if((Get-Variable 'VCPKG_ROOT' -ErrorAction SilentlyContinue) -eq $null){
-    $env:VCPKG_ROOT = "./scripts/vcpkg/"
-}
+Powershell.exe ./setEnv.ps1
 $env:PATH = "$env:VCPKG_ROOT;$env:PATH"
 cmake -S $SOURCE_PATH --preset win64-default
 Read-Host -Prompt "Press Enter to exit"
